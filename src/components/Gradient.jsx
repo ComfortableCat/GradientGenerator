8d4973a9fbe51ba74b0fc5a44f9c0adfed63642c
Bertie Raffle <bertierwork@gmail.com>
2024-12-21 22:25:33 +0000

    add minimise for menu

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -1,7 +1,9 @@
 "use client";
+import Image from "next/image";
 import { useRef, useState } from "react";
 
 export default function Gradient() {
+  const [minimise, setMinimise] = useState(false);
   const [background, setBackground] = useState({
     type: "linear",
     direction: "45",
@@ -50,98 +52,114 @@ export default function Gradient() {
             : ""
         } ${cssColoursList})`,
       }}
-      className={`w-[100vw] h-[100vh] flex flex-col justify-center content-center `}
+      className={`w-[100vw] h-[100vh] flex flex-col ${
+        !minimise ? `justify-center` : `justify-end`
+      }  content-center `}
     >
-      <div className="flex flex-col mx-auto bg-white rounded-md w-[230px] px-3 py-2 border-2 border-black">
+      <div className="flex flex-col mx-auto bg-white rounded-md w-[230px] px-3 py-2 border-2 border-black relative">
         <h1>Choose your Gradient</h1>
-        <select name="type" onChange={handleChange}>
-          <option value="linear">Linear</option>
-          <option value="radial">Radial</option>
-          <option value="conic">Conic</option>
-        </select>
-        {background.type === "linear" ? (
-          <div>
-            <label htmlFor="directionInput">Direction: </label>
-            <input
-              name="direction"
-              id="directionInput"
-              type="range"
-              max={360}
-              min={0}
-              step={1}
-              defaultValue={background.direction}
-              onChange={handleChange}
-            />
-          </div>
-        ) : background.type === "radial" ? (
+        <Image
+          src={"/up-arrow.png"}
+          alt="arrow"
+          width={20}
+          height={20}
+          className={`absolute top-2 right-4 ${!minimise ? `rotate-180` : ``}`}
+          onClick={() => setMinimise(!minimise)}
+        />
+        {!minimise && (
           <>
-            <select name="shape" onChange={handleChange}>
-              <option value={"ellipse"}>ellipse</option>
-              <option value={"circle"}>circle</option>
+            <select name="type" onChange={handleChange}>
+              <option value="linear">Linear</option>
+              <option value="radial">Radial</option>
+              <option value="conic">Conic</option>
             </select>
-            <p>X-Coordinate</p>
-            <input
-              name="xCoordinate"
-              type="range"
-              value={background.center[0]}
-              onChange={(event) => {
-                const newCenter = {
-                  center: [event.target.value, background.center[1]],
-                };
-                setBackground({ ...background, ...newCenter });
-              }}
-            />
-            <p>Y-Coordinate</p>
-            <input
-              name="yCoordinate"
-              type="range"
-              value={background.center[1]}
-              onChange={(event) => {
-                const newCenter = {
-                  center: [background.center[0], event.target.value],
-                };
-                setBackground({ ...background, ...newCenter });
-              }}
-            />
-          </>
-        ) : (
-          <></>
-        )}
+            {background.type === "linear" ? (
+              <div>
+                <label htmlFor="directionInput">Direction: </label>
+                <input
+                  name="direction"
+                  id="directionInput"
+                  type="range"
+                  max={360}
+                  min={0}
+                  step={1}
+                  defaultValue={background.direction}
+                  onChange={handleChange}
+                />
+              </div>
+            ) : background.type === "radial" ? (
+              <>
+                <select name="shape" onChange={handleChange}>
+                  <option value={"ellipse"}>ellipse</option>
+                  <option value={"circle"}>circle</option>
+                </select>
+                <p>X-Coordinate</p>
+                <input
+                  name="xCoordinate"
+                  type="range"
+                  value={background.center[0]}
+                  onChange={(event) => {
+                    const newCenter = {
+                      center: [event.target.value, background.center[1]],
+                    };
+                    setBackground({ ...background, ...newCenter });
+                  }}
+                />
+                <p>Y-Coordinate</p>
+                <input
+                  name="yCoordinate"
+                  type="range"
+                  value={background.center[1]}
+                  onChange={(event) => {
+                    const newCenter = {
+                      center: [background.center[0], event.target.value],
+                    };
+                    setBackground({ ...background, ...newCenter });
+                  }}
+                />
+              </>
+            ) : (
+              <></>
+            )}
 
-        {background.colours.map((colour, i) => {
-          return (
-            <div key={i} className="flex justify-between items-center">
-              <p>Colour {i + 1}:</p>
-              <input
-                type="color"
-                name={`col${i + 1}`}
-                defaultValue={background.colours[i]}
-                className="w-8 h-8 m-0 p-0"
-                onChange={(event) => handleColourChange(event.target.value, i)}
-              />
-            </div>
-          );
-        })}
-        <button
-          onClick={() => {
-            const newColourInput = {
-              colours: [...background.colours, "#000000"],
-            };
-            setBackground({ ...background, ...newColourInput });
-          }}
-        >
-          Add colour
-        </button>
-        {background.colours.length > 2 && (
-          <button
-            onClick={() => {
-              const updatedColours = { colours: [...background.colours] };
-              updatedColours.colours.pop();
-              setBackground({ ...background, ...updatedColours });
-            }}
-          >
-            Remove Colour
-          </button>
+            {background.colours.map((colour, i) => {
+              return (
+                <div key={i} className="flex justify-between items-center">
+                  <p>Colour {i + 1}:</p>
+                  <input
+                    type="color"
+                    name={`col${i + 1}`}
+                    defaultValue={background.colours[i]}
+                    className="w-8 h-8 m-0 p-0"
+                    onChange={(event) =>
+                      handleColourChange(event.target.value, i)
+                    }
+                  />
+                </div>
+              );
+            })}
+            <button
+              onClick={() => {
+                const newColourInput = {
+                  colours: [...background.colours, "#000000"],
+                };
+                setBackground({ ...background, ...newColourInput });
+              }}
+            >
+              Add colour
+            </button>
+            {background.colours.length > 2 && (
+              <button
+                onClick={() => {
+                  const updatedColours = { colours: [...background.colours] };
+                  updatedColours.colours.pop();
+                  setBackground({ ...background, ...updatedColours });
+                }}
+              >
+                Remove Colour
+              </button>
+            )}
+          </>
         )}
       </div>
     </div>
